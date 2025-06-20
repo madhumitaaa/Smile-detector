f349d322e58285bb4eeec80ea6cdef5f97a0d03d
madhumitaaa <madhumita.mandal.ee26@heritageit.edu.in>
2025-06-20 10:47:28 +0530

    added html css and javascript and faceapi

---
+++ script.js
@@ -0,0 +1,54 @@
+const video=document.getElementById("video")
+Promise.all(
+    [
+        faceapi.nets.tinyFaceDetector.loadFromUri("models"),     
+         faceapi.nets.faceLandmark68Net.loadFromUri("models"),     
+         faceapi.nets.faceExpressionNet.loadFromUri("models")
+    ]
+).then(startVideo);
+
+
+function startVideo()
+{
+    navigator.mediaDevices.getUserMedia({video:{}})
+    .then((stream)=>{
+        video.srcObject=stream;
+
+    })
+
+    .catch((err)=>console.log("camera error:",err));
+}
+video.addEventListener("play",()=>{
+    const canvas=faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+
+    const displaySize={width:video.width,height:video.height};
+    faceapi.matchDimensions(canvas,displaySize);
+
+
+setInterval(async()=>{
+    const detections=await faceapi.detectAllFaces(
+        video,new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+
+const resizedDetections=faceapi.resizeResults(detections,displaySize);
+canvas.getContext("2d").clearRect(0,0,canvas.width,canvas.height);
+faceapi.draw.drawDetections(canvas,resizedDetections);
+faceapi.draw.drawFaceExpressions(canvas,resizedDetections);
+
+resizedDetections.forEach(d=>{
+    if(d.expressions.happy>0.7){
+        console.log("😊 You are smiling!");
+    }
+});
+
+},200);
+
+});
+
+
+
+
+
+
